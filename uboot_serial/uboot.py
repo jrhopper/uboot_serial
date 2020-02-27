24dab45ef2171831c8e87b819112aa8a3b555c59
Steven Lowery <Steven.Lowery011@gmail.com>
2020-02-27 16:15:29 -0500

    completed bootloader script.

---
+++ uboot_serial/uboot.py
@@ -0,0 +1,213 @@
+"""
+uboot.py
+This script contains supporting functions for updating firmware on the device.
+"""
+
+import time
+import datetime
+
+def read_until(com, string, timeout=5):
+    """
+    Read character by character until the desired string is found up until the timeout in seconds.
+    Prints all characters read.
+
+    Parameters:
+        com                 The serial port
+        string              The desired search string
+        timeout             The amount of time to search in seconds
+    Returns:
+        (string)            Returns text if the string was found or None if timeout elapsed.
+    """
+    text = ""
+    start = time.time()
+    com.reset_input_buffer()
+    while (time.time() - start) < timeout:
+        char = com.read().decode('ascii', 'ignore')
+        text += char
+        if string in text:
+            print(text)
+            return text
+    print(text)
+    return None
+
+def readline_until(com, string, timeout=5):
+    """
+    Read line by line until the desired string is found up until the timeout in seconds.
+    Prints all characters read.
+
+    Parameters:
+        com                 The serial port
+        string              The desired search string
+        timeout             The amount of time to search in seconds
+    Returns:
+        (string)            Returns text if the string was found or None if timeout elapsed.
+    """
+    text = ""
+    start = time.time()
+    com.reset_input_buffer()
+    while (time.time() - start) < timeout:
+        line = com.readline().decode('ascii', 'ignore')
+        text += line
+        if string in text:
+            print(text)
+            return text
+    print(text)
+    return None
+
+def send_cmd(com, command):
+    """
+    Send a command at the prompt (u-boot or root). Does not wait for next prompt.
+
+    Parameters:
+        com                 The serial port
+        command             The command string to send at prompt
+    Returns:
+        none
+    """
+    command = command + "\r\n"
+    com.write(command.encode('ascii'))
+
+def check_prompt(com):
+    """
+    Checks if a prompt exists and if so, which prompt (u-boot, login, or root).
+    Prints all characters read.
+
+    Parameters:
+        com                 The serial port
+    Returns:
+        (str)               A string value: "uboot" | "login" | "root"
+    """
+    send_cmd(com, "")
+    send_cmd(com, "")
+    if readline_until(com, "=>", timeout=1):
+        return "uboot"
+    send_cmd(com, "")
+    send_cmd(com, "")
+    if readline_until(com, "ccimx6sbc login:", timeout=1):
+        return "login"
+    send_cmd(com, "")
+    send_cmd(com, "")
+    if readline_until(com, "root@ccimx6sbc:", timeout=1):
+        return "root"
+
+def boot_to_uboot(com):
+    """
+    Checks prompt and reboots appropriately. Stops autoboot. Waits for uboot prompt.
+    Prints all characters read.
+
+    Parameters:
+        com                 The serial port
+    Returns:
+        (boolean)           Returns True if uboot prompt is ready or False if not.
+    """
+    prompt = check_prompt(com)
+    if prompt == "uboot":
+        send_cmd(com, "reset")
+    elif prompt == "login":
+        success = login(com)
+        if success:
+            send_cmd(com, "reboot")
+    elif prompt == "root":
+        send_cmd(com, "reboot")
+    else:
+        return False
+    read_until(com, "Hit any key to stop autoboot: ", timeout=20)
+    send_cmd(com, "")
+    prompt = check_prompt(com)
+    if prompt == "uboot":
+        return True
+    return False
+
+def login(com, root_psswd="Allergen_lock"):
+    """
+    Logs in as root user. Assumes already at login prompt.
+
+    Parameters:
+        com                 The serial port
+    Returns:
+        (boolean)           Returns True if root login succeeds. Returns False if not.
+    """
+    send_cmd(com, "root")
+    read_until(com, "Password:")
+    send_cmd(com, root_psswd)
+    prompt = check_prompt(com)
+    if prompt == "root":
+        return True
+    return False
+
+def boot_to_login(com):
+    """
+    Checks prompt and reboots appropriately. Waits for kernel startup.
+    Stops at login prompt.
+    Prints all characters read.
+
+    Parameters:
+        com                 The serial port
+    Returns:
+        (boolean)           Returns True if login prompt is ready or False if not.
+    """
+    prompt = check_prompt(com)
+    if prompt == "uboot":
+        send_cmd(com, "reset")
+    elif prompt == "login":
+        return True
+    elif prompt == "root":
+        send_cmd(com, "exit")
+    else:
+        return False
+    read_until(com, "ccimx6sbc login: ", timeout=40)
+    send_cmd(com, "")
+    prompt = check_prompt(com)
+    if prompt == "login":
+        return True
+    return False
+
+def boot_to_root(com, root_psswd="Allergen_lock"):
+    """
+    Checks prompt and reboots appropriately. Waits for kernel startup. Logs in as root.
+    Stops at root prompt.
+    Prints all characters read.
+
+    Parameters:
+        com                 The serial port
+        root_psswd          Root password. Default = "Allergen_lock"
+    Returns:
+        (boolean)           Returns True if root prompt is ready or False if not.
+    """
+    prompt = check_prompt(com)
+    if prompt == "uboot":
+        send_cmd(com, "reset")
+    elif prompt == "login":
+        success = login(com, root_psswd=root_psswd)
+        if success:
+            return True
+    elif prompt == "root":
+        return True
+    else:
+        return False
+    read_until(com, "ccimx6sbc login: ", timeout=40)
+    send_cmd(com, "")
+    prompt = check_prompt(com)
+    if prompt == "login":
+        success = login(com, root_psswd=root_psswd)
+        if success:
+            return True
+    else:
+        return False
+
+def log(text, print_log=False):
+    """
+    Similar to print(), but with timestamping. If print_log is True, then log is printed
+    to terminal. Returns log_data with a timestamp appended.
+
+    Parameters:
+        text                The string to be logged
+        print_log           If True, prints to terminal. Default=False
+    Returns:
+        log_data            The string of timestampped log data
+    """
+    timestamp = datetime.datetime.now().strftime("%b-%d-%Y-%H-%M-%S").upper()
+    log_data = "[{}]    ".format(timestamp) + text
+    if print_log:
+        print(log_data)
+    return log_data
---
+++ uboot_serial/update_bootloader.py
@@ -0,0 +1,158 @@
+"""
+update_bootloader.py
+This script updates the bootloader image on the u-boot device taken from the microSD card.
+"""
+
+import argparse
+import serial
+import serial.tools.list_ports
+from uboot import read_until, readline_until, send_cmd, check_prompt, boot_to_uboot, \
+    login, boot_to_login, boot_to_root, log
+
+def main():
+    """
+    Main function. Executed when run from command line.
+    """
+    in_arg = get_input_args()
+
+    # validate inputs
+    if in_arg.port == "":
+        # scan for FTDI com ports by vendor ID = 0x0403
+        usb_serial_ports = []
+        for port in serial.tools.list_ports.grep("0403"):
+            usb_serial_ports.append(port.device)
+        if usb_serial_ports == []:
+            # no devices found
+            print("Could not find an FTDI connected device.")
+            print("Please connect an FTDI USB-to-serial converter " \
+                  "or specify the port with the --port option.")
+            print("Exiting script...")
+            return
+        if len(usb_serial_ports) > 1:
+            # found multiple devices
+            print("Found multiple FTDI devices: {}".format(usb_serial_ports))
+            print("Please use the --port option to select the correct device.")
+            print("Exiting script...")
+            return
+        ftdi_port = usb_serial_ports[0]
+    else:
+        ftdi_port = in_arg.port
+
+    # run script
+    update_bootloader(ftdi_port, in_arg.image)
+
+def update_bootloader(port, image):
+    """
+    update_bootloader function
+    This function updates the bootloader from the image on the microSD card.
+    The procedure is as follows:
+    1. Reboot the device to stop at u-boot prompt.
+       If no prompt is found, the user is asked to boot the device from the SD card.
+    2. Program the bootloader on the device from image on card.
+    3. Reset the CPU and come up to new u-boot prompt.
+
+    Parameters
+        port                The COM port on which the FTDI serial device is connected.
+                            On Windows machines, this will be the word COM followed by
+                            a port number.
+        image               The name of the u-boot image file to be flashed
+    Returns
+        none
+    """
+    print_log = False
+    if __name__ == '__main__':
+        print_log = True
+
+    # validate inputs
+    if image[-4:] != ".imx":
+        raise ValueError("Image file extension must be *.imx")
+    try:
+        com = serial.Serial(port, 115200, timeout=1)
+    except serial.SerialException:
+        print("Could not access comport {}".format(port))
+        print("Exiting script...")
+        return
+
+    # Check prompt to find where we're at
+    prompt = check_prompt(com)
+
+    # If no prompt, then tell user to boot from SD card
+    if prompt is None:
+        log("Please boot the device via the SD card now...", print_log)
+        result = read_until(com, "Hit any key to stop autoboot: ", timeout=60)
+        if result:
+            send_cmd(com, "")
+            prompt = check_prompt(com)
+            if prompt == "uboot":
+                log("You may release the programming button now...", print_log)
+        else:
+            log("Request timed out...", print_log)
+            print("Exiting script...")
+            return
+    # Stop at u-boot prompt
+    else:
+        log("Booting to u-boot prompt...", print_log)
+        boot_to_uboot(com)
+
+    # Program the bootloader
+    log("Programming the bootloader...", print_log)
+    command = "update uboot mmc 1 fat " + image
+    send_cmd(com, command)
+    read_until(com, "program the boot loader? <y/N>")
+    send_cmd(com, "y")
+    result = readline_until(com, "=>")
+    if "Update was successful" in result:
+        log("Success!", print_log)
+        log("Bootloader updated successfully.", print_log)
+    else:
+        log("Failure!", print_log)
+        if "Error loading firmware file to RAM" in result:
+            log("Could not find u-boot image file {} on SD card, " \
+                "or card is not present.".format(image), print_log)
+        print("Exiting script...")
+        return
+
+    # Reset and stop at u-boot prompt
+    log("Resetting CPU and booting to u-boot prompt...", print_log)
+    boot_to_uboot(com)
+
+    com.close()
+    log("Bootloader update is complete.", print_log)
+
+def get_input_args():
+    """
+    Parse command line arguments.
+
+    usage: update_bootloader.py [-h] [--port PORT] [--image IMAGE]
+
+    positional arguments:
+        none
+
+    optional arguments:
+        -h, --help          show this help message and exit
+        --port PORT         The COM port on which the FTDI serial device is connected.
+                            On Windows machines, this will be the word COM followed by
+                            a port number. By default, this script scans for FTDI
+                            devices and selects one if available.
+        --image IMAGE       The name of the u-boot image file to be flashed;
+                            default=u-boot-ccimx6qsbc.imx
+
+    Parameters:
+        none
+    Returns:
+        parse_args() - data structure that stores the command line arguments object
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', action="store", type=str, default="",
+                        help='The COM port on which the FTDI serial device is ' \
+                            'connected. On Windows machines, this will be the ' \
+                            'word COM followed by a port number. ' \
+                            'By default, this script scans for FTDI devices ' \
+                            'and selects one if available.')
+    parser.add_argument('--image', action="store", type=str, default='u-boot-ccimx6qsbc.imx',
+                        help='The name of the u-boot image file to be flashed; ' \
+                             'default=u-boot-ccimx6qsbc.imx')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    main()
